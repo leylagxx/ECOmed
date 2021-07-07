02a4b89f0c9679817d6764eee6e377f7ad2ab16e
leylagxx <81859929+leylagxx@users.noreply.github.com>
2021-07-07 19:49:28 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,34 @@
+
+
+let modal = document.getElementById('myModal');
+
+
+let btn = document.getElementById("myBtn");
+let btn1 = document.getElementById("myBtn1");
+
+
+let span = document.getElementsByClassName("cls")[0];
+let span1 = document.getElementsByClassName("cls1")[0];
+
+btn.onclick = function() {
+    modal.style.display = "block";
+}
+btn1.onclick = function() {
+    modal.style.display = "block";
+}
+
+span.onclick = function() {
+    modal.style.display = "none";
+}
+span1.onclick = function() {
+    modal.style.display = "none";
+}
+
+
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+
+
